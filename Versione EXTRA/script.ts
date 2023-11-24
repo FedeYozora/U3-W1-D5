58d9a04579a4f8663f61631a270019d77eb7cc84
FedeYozora <federicobonfi@gmail.com>
2023-11-24 15:46:28 +0100

    added an UI similar to smartphone to simulate

---
+++ Versione EXTRA/script.ts
@@ -0,0 +1,104 @@
+let callN1 = document.getElementById("call1")! as HTMLElement;
+let callN2 = document.getElementById("call2")! as HTMLElement;
+let callN3 = document.getElementById("call3")! as HTMLElement;
+let resultsN1 = document.getElementById("results1")! as HTMLElement;
+let resultsN2 = document.getElementById("results2")! as HTMLElement;
+let resultsN3 = document.getElementById("results3")! as HTMLElement;
+let resultsDiv = document.getElementById("callResults")! as HTMLElement;
+
+interface Smartphone {
+  ricarica(unaRicarica: number): void;
+  chiamata(minutiDurata: number): void;
+  numero404(): number;
+  getNumeroChiamate(): number;
+  azzeraChiamate(): void;
+}
+class User implements Smartphone {
+  carica: number;
+  numeroChiamate: number;
+
+  constructor() {
+    this.carica = 0;
+    this.numeroChiamate = 0;
+  }
+
+  ricarica(unaRicarica: number): void {
+    this.carica += unaRicarica;
+  }
+
+  chiamata(minutiDurata: number): void {
+    this.carica -= minutiDurata * 0.2;
+    this.numeroChiamate++;
+  }
+
+  numero404(): number {
+    return this.carica;
+  }
+
+  getNumeroChiamate(): number {
+    return this.numeroChiamate;
+  }
+
+  azzeraChiamate(): void {
+    this.numeroChiamate = 0;
+  }
+}
+
+function logMessage(message) {
+  resultsDiv.innerHTML += message + "<br>";
+}
+
+const firstUser = new User();
+firstUser.ricarica(40);
+callN1.addEventListener("click", function () {
+  if (firstUser.carica > 0.2) {
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
+    firstUser.chiamata(randomNumber);
+    logMessage("Saldo Rimanente Mario: " + firstUser.numero404().toFixed(2));
+  } else {
+    alert("Mario non ha credito sufficiente");
+  }
+});
+
+resultsN1.addEventListener("click", function () {
+  logMessage("Chiamate effettuate da Mario: " + firstUser.getNumeroChiamate());
+  firstUser.azzeraChiamate();
+  console.log("Numero Chiamate da Mario after reset:", firstUser.getNumeroChiamate());
+});
+
+const secondUser = new User();
+secondUser.ricarica(20);
+callN2.addEventListener("click", function () {
+  if (secondUser.carica > 0.2) {
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
+    secondUser.chiamata(randomNumber);
+    logMessage("Saldo Rimanente Luigi: " + secondUser.numero404().toFixed(2));
+  } else {
+    alert("Luigi non ha credito sufficiente");
+  }
+});
+
+resultsN2.addEventListener("click", function () {
+  logMessage("Chiamate effettuate da Luigi: " + secondUser.getNumeroChiamate());
+  secondUser.azzeraChiamate();
+  console.log("Numero Chiamate da Luigi after reset:", secondUser.getNumeroChiamate());
+});
+
+const thirdUser = new User();
+thirdUser.ricarica(30);
+
+callN3.addEventListener("click", function () {
+  if (thirdUser.carica > 0.2) {
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
+    thirdUser.chiamata(randomNumber);
+    logMessage("Saldo Rimanente Peach: " + thirdUser.numero404().toFixed(2));
+  } else {
+    alert("Peach non ha credito sufficiente");
+  }
+});
+
+resultsN3.addEventListener("click", function () {
+  logMessage("Chiamate effettuate da Peach: " + thirdUser.getNumeroChiamate());
+  thirdUser.azzeraChiamate();
+  console.log("Numero Chiamate da Peach after reset:", thirdUser.getNumeroChiamate());
+});
